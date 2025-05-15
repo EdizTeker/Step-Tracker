13b9d07c7b6deb4cf5f543bf3f84dd6406841f07
EdizTeker <edizteker5@gmail.com>
2025-05-15 13:23:29 +0300

    Step Sensor implemented - Version 2

--- app/src/main/java/com/hehe/steptracker/view/MainActivity.java
+++ app/src/main/java/com/hehe/steptracker/view/MainActivity.java
@@ -3,7 +3,13 @@ package com.hehe.steptracker.view;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import android.content.pm.PackageManager;
+
+import android.os.Build;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -17,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     private MainViewModel viewModel;
     private TextView stepTxt;
+    private static final int PERMISSION_REQUEST_ACTIVITY_RECOGNITION = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +35,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (checkSelfPermission(android.Manifest.permission.ACTIVITY_RECOGNITION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{android.Manifest.permission.ACTIVITY_RECOGNITION}, PERMISSION_REQUEST_ACTIVITY_RECOGNITION);
+            }
+        }
+
         viewModel = new ViewModelProvider(this).get(MainViewModel.class);
         stepTxt = findViewById(R.id.stepTxt);
+        viewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication())).get(MainViewModel.class);
         viewModel.getStepForActivity().observe(this, stepCount -> {
             stepTxt.setText(stepCount);
         });
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_ACTIVITY_RECOGNITION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/hehe/steptracker/viewModel/MainViewModel.java
+++ app/src/main/java/com/hehe/steptracker/viewModel/MainViewModel.java
@@ -1,20 +1,74 @@
 package com.hehe.steptracker.viewModel;
 
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.app.Application;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import com.hehe.steptracker.model.StepsModel;
 
-public class MainViewModel extends ViewModel {
+public class MainViewModel extends AndroidViewModel implements SensorEventListener {
 
 
     private MutableLiveData<String> stepCount;
-    public MainViewModel() {
+    private SensorManager sensorManager;
+    private Sensor stepSensor;
+
+    public MainViewModel(Application application) {
+        super(application);
         stepCount = new MutableLiveData<>();
-        stepCount.setValue("deneme");
+        initializeSensor();
+        //stepCount.setValue("0");
+        registerSensor();
+
+
+
     }
-    
     public LiveData<String> getStepForActivity(){
         return stepCount;
     }
+
+    public void initializeSensor(){
+        // Define sensor and sensor manager
+        sensorManager = (SensorManager) getApplication().getSystemService(Context.SENSOR_SERVICE);
+        if (sensorManager != null) {
+            stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            if (stepSensor != null) {
+                Log.d("Step Counter", "Step Sensor Found and InitialIZED.");
+            } else {
+                Log.e("Step Counter", "Step Sensor IS NULL (Not found on device).");
+            }
+        } else {
+            Log.e("Step Counter", "SensorManager IS NULL.");
+        }
+    }
+    private void registerSensor() {
+        if (stepSensor != null) {
+            sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI);
+            Log.d("Step Counter", "Step Sensor Registered");
+        }
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event == null) return;
+        if (event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            stepCount.setValue(String.valueOf(event.values[0]));
+        }
+        Log.d("Step Counter", "Sensor event received: " + event.values[0]);
+
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
 }
